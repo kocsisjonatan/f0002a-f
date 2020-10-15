bf0fee657133357388a7692627b3a3f69bc3a62c
kocsisjonatan <54977355+kocsisjonatan@users.noreply.github.com>
2020-10-15 12:38:46 +0200

    Add files via upload

---
+++ f0001b.py
@@ -0,0 +1,4 @@
+print('Rásír a sok kis malac is!')
+print('"Én is, én is, én is, én is!"')
+
+#ki tudjuk írni az idéző jelet
---
+++ f0002c.py
@@ -0,0 +1,7 @@
+print('Első sor\n')
+print('\nNos?')
+print('Azaz?\n')
+print('\nAkkor most mennyi?\n\n')
+print('Utolsó sor')
+
+#5sor ki írás 4sor üres 
